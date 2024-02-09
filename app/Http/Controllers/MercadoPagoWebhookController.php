8f884e58c4de8c15656d5ea2cedc1a8f87470dbb
avillaSori <155116601+avillaSori@users.noreply.github.com>
2024-02-08 21:24:58 -0600

    creando vusta para los logs

--- app/Http/Controllers/MercadoPagoWebhookController.php
+++ app/Http/Controllers/MercadoPagoWebhookController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
 
 class MercadoPagoWebhookController extends Controller
 {
@@ -11,11 +12,24 @@ class MercadoPagoWebhookController extends Controller
         // Obtener el evento del webhook
         $data = $request->all();
 
-        // Procesar el evento
-        // Tu lógica para manejar los eventos de MercadoPago aquí
+    // Procesar el evento
+    // Tu lógica para manejar los eventos de MercadoPago aquí
 
-        // Responder al servidor de MercadoPago con un código 200
-        return response()->json(['status' => 'OK'], 200);
+    // Guardar la respuesta en el archivo de logs
+    Log::info('Respuesta del webhook de MercadoPago: ' . json_encode($data));
+
+    // Responder al servidor de MercadoPago con un código 200
+    return response()->json(['status' => 'OK'], 200);
     }
+
+    public function viewLogs()
+{
+    // Obtener el contenido del archivo de logs
+    $logContent = File::get(storage_path('logs/laravel.log'));
+
+    // Mostrar el contenido en una vista
+    return view('logs', ['logContent' => $logContent]);
+}
+
 }
 
