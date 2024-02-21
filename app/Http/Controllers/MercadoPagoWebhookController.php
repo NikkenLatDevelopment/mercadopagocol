0f9637496869078beb4a3ec87a31b8ca0d9668f4
avillaSori <155116601+avillaSori@users.noreply.github.com>
2024-02-21 01:06:13 -0600

    subiendo inserecion a bd comentada hasta comprobar que el env productivo funcione

--- app/Http/Controllers/MercadoPagoWebhookController.php
+++ app/Http/Controllers/MercadoPagoWebhookController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
-
 use Illuminate\Support\Facades\Log;
-
+use App\Models\WebhookLog; 
+use Illuminate\Support\Str;
 
 
 class MercadoPagoWebhookController extends Controller
@@ -20,6 +20,16 @@ class MercadoPagoWebhookController extends Controller
         return response()->json(['success' => true]);
         */
         /*funcion para log general*/
+
+         // Guardar los datos en la base de datos
+   /*
+        $webhookLog = new WebhookLog();    
+        $webhookLog->payment_method = $data['data']['type'] ?? null; // Puedes ajustar este valor según tus necesidades
+        $webhookLog->log_description = 'Webhook Response'; // Puedes ajustar este valor según tus necesidades
+        $webhookLog->log_data = Str::limit(json_encode($data), 255); // Limitar la longitud de la cadena a 255 caracteres para SQL Server
+        $webhookLog->save();
+*/
+
 
         $data = $request->all();
         Log::info('Webhook Response', $data); // Utilizando el logger por defecto
